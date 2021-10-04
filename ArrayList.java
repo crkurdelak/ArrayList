4e7064ce7ad39757ff2fd95d8b79d7ca303cc8f2
crkur <ckurdelak20@georgefox.edu>
2021-10-03 21:16:07 -0700

    stubbed out all methods, implemented isEmpty

--- ArrayList.java
+++ ArrayList.java
@@ -8,7 +8,7 @@
 public class ArrayList<E> {
     // TODO implement class
 
-    private E[] _backingArray;
+    private Object[] _values;
     // a bookkeeping variable that keeps track of the highest filled index in the backing array
     private int _index;
 
@@ -19,7 +19,7 @@ public class ArrayList<E> {
      * @param initialCapacity the initial capacity of this ArrayList's backing array
      */
     public ArrayList(int initialCapacity) {
-        _backingArray = new E[initialCapacity];
+        _values = new Object[initialCapacity];
         _index = 0;
     }
 
@@ -29,7 +29,7 @@ public class ArrayList<E> {
      */
     public ArrayList() {
         this(10);
-    }
+    } // TODO make 10 a constant
 
 
     /**
@@ -43,19 +43,145 @@ public class ArrayList<E> {
 
 
     /**
+     * Returns true if this list contains no elements.
+     *
+     * @return true if this list contains no elements
+     * false if this list contains elements
+     */
+    public boolean isEmpty() {
+        // TODO implement isEmpty
+        boolean result;
+        if (_index == 0) {
+            result = true;
+        }
+        else if (_index > 0) {
+            result = false;
+        }
+        else {
+            // TODO see if you need to throw an exception here
+        }
+
+        return result;
+    }
+
+
+    /**
      * Returns the element at the specified position in this list.
      *
      * @param index the index of the element to return
      * @return the element at the specified position in this list
      * @throws IndexOutOfBoundsException if the index is out of range (index < 0 || index >= size)
      */
+    @SuppressWarnings("unchecked")
     public E get(int index) {
         if (index > 0 && index < _index) {
-            return _backingArray[index];
+            return (E)_values[index];
+            // ONLY cast right before return
         }
         else {
             throw new IndexOutOfBoundsException();
         }
     }
 
+
+    /**
+     * TODO write javadoc comments
+     * @param index
+     * @param element
+     */
+    public void add(int index, E element) {
+        // validate index
+        // TODO shift values to right
+        // _values[index] = value;
+    }
+
+
+    /**
+     * TODO write javadoc
+     * @param element
+     * @return true if this collection has changed as a result of the call
+     */
+    public boolean add(E element) {
+        add(_index, element);
+        return true;
+    }
+
+
+    /**
+     * Removes and returns the element from the specified position in this list.
+     * Shifts any subsequent elements to the left (subtracts one from their indices)
+     *
+     * @param index the index of the element to be removed
+     * @return the element that was removed from the list
+     */
+    public E remove(int index) {
+        // TODO implement remove
+        // validate index
+        // should we use set method?
+    }
+
+
+    /**
+     * Removes all the elements from this list. The list will be empty after this call returns.
+     */
+    public void clear() {
+        // TODO implement clear
+        // use remove method
+    }
+
+
+    /**
+     * Replaces the element at the specified position in this list with the specified element.
+     *
+     * @param index the index of the element to replace
+     * @param element the element to be stored at the specified position
+     * @return the element previously at the specified position
+     */
+    public E set(int index, E element) {
+        // TODO implement set
+        // validate index
+    }
+
+
+    /**
+     * Returns the index of the first occurrence of the specified element in this list,
+     * or -1 if this list does not contain the element.
+     *
+     * More formally, returns the lowest index i such that
+     * Objects.equals(o, get(i)), or -1 if there is no such index.
+     *
+     * @param element the element to search for
+     * @return the index of the first occurrence of the specified element in this list,
+     * or -1 if this list does not contain the element.
+     */
+    public int indexOf(E element) {
+        // TODO implement indexOf
+        // linear search
+    }
+
+
+    // TODO write private helper method to grow array
+
+    /**
+     * Grows the array by TODO figure out growing strategy
+     */
+    private void growArray() {
+
+    }
+
+    // TODO write private helper method to shift elements
+    /**
+     * Shifts elements
+     * @param index
+     * @param direction
+     */
+    private void shiftElements(int index, int direction) {
+        // if 1, then shift to right
+        // for (int i = _index; i <= index; i--)
+        // _values[i + 1] = values[i];
+
+        // if -1, shift to left
+        // for (int i = index; i <= _index; i++)
+        // _values[i] = values[i + 1];
+    }
 }
